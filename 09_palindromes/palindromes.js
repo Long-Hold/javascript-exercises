f62862d7f97f8fb3b2f7c4c49e6f440463c5377a
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-04-20 12:25:36 -0400

    fix: Function now ignore punctuation

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,9 +1,11 @@
 const palindromes = function (word) {
-    // Split the original word into an array,
-    // reverse the array, then join it back into a string
-    const reversedWord = word.split('').reverse().join('');
+    // Remove any punctuation from the original string
+    const noPunctuationWord = word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
 
-    return reversedWord === word;
+    // Split the string into an array, reverse it, then join it back into a string
+    const reversedWord = noPunctuationWord.split().reverse().join('');
+
+    return reversedWord === noPunctuationWord;
 };
 
 // Do not edit below this line
