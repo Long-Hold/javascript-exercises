9a3f1fdc8465b7dd62a2b1e1b6b72bf64435b80a
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-04-20 12:17:44 -0400

    feat: Implement first palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    // Split the original word into an array,
+    // reverse the array, then join it back into a string
+    const reversedWord = word.split('').reverse().join('');
 
+    return reversedWord === word;
 };
 
 // Do not edit below this line
