e78b2d8a2e537fb3905f08c3da47a274a5b12268
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-02-03 14:19:01 -0500

    feat: Implement my recursive solution for contains

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,5 +1,16 @@
-const contains = function() {
-  
+const contains = function(obj, value) {
+    let foundValue = false;
+    for (const key of Object.keys(obj)) {
+        if (foundValue === true) break;
+
+        if (Number.isNaN(obj[key]) && Number.isNaN(value)) foundValue = true;
+        if (obj[key] === value) foundValue = true;
+
+        // If the value is another object, we must begin the recursion
+         else if (typeof obj[key] === 'object' && obj[key] !== null) foundValue = contains(obj[key], value);
+    }
+
+    return foundValue;
 };
   
 // Do not edit below this line
