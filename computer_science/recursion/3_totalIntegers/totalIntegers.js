a223ef965987cf7091fd9410cefe73f967780cd3
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-02-03 16:27:06 -0500

    feat: Implement totalIntegers function that satisfies tests

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,18 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function(collection) {
+    if (!isObject(collection)) return;
+
+    let intCount = 0;
+    Object.values(collection).forEach((value) => {
+        if (Number.isInteger(value)) ++intCount;
+        if (isObject(value)) intCount += totalIntegers(value);
+    });
+
+    return intCount;
 };
+
+function isObject(data) {
+    return (typeof data === 'object' && data !== null);
+}
   
 // Do not edit below this line
 module.exports = totalIntegers;
