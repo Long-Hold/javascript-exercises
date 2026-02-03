2b38ef1507f02a5f7bcec377b73abdd9a8b7bc69
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-02-03 13:57:03 -0500

    feat: Implement factorial recursive function that passes all tests

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,9 @@
-const factorial = function() {
+const factorial = function(num) {
+    if (!Number.isInteger(num)) return undefined;
+    if (num < 0) return undefined;
 
+    if (num <= 1) return 1;
+    return num * factorial(num - 1);
 };
 
 // Do not edit below this line
